8694378649e4e9e7614ccdbd043ce623fadf4a35
Robert Bubnik <rbubnik@gmail.com>
2023-10-08 10:25:24 -0400

    passing all tests

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  
+  let [position, switchPosition] = useState('')
+
+  function AddtoCart () {
+    
+   if (position === '') {
+
+    return switchPosition(position = 'in-cart')
+   }
+   if (position === 'in-cart') {
+    return switchPosition(position='')
+   }
+  }
+  let add = 'Add to Cart'
+  
+  position =='' ? add = 'Add to Cart': add ='Remove from Cart'
+  
   return (
-    <li className="">
+    <li className={position}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+      onClick ={()=>AddtoCart()}
+      className="add">{add}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+let [selectedCategory, switchFilter] = useState('All')
+
+
+function filterFunction(event) {
+  switchFilter(event.target.value)
+  
+}
+console.log(selectedCategory)
+
+const foodsToDisplay = items.filter((food)=>{
+  if (selectedCategory == 'All') {
+    return true
+  } else {
+    return food.category === selectedCategory
+  }
+
+})
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select
+         onChange={(event)=>filterFunction(event)}
+         name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +34,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {foodsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
